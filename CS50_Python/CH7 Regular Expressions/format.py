1f1626512d8eb3e91c1c39d23a1f389a8f32f6b4
KaushalCXXAI <codecrusher0@gmail.com>
2025-06-12 23:24:40 +0530

    Day 13 Object-Oriented Programmin class, __init__, __str__, @property, @classmethod, @staticmethod, object.__add__

---
+++ CS50_Python/CH7 Regular Expressions/format.py
@@ -0,0 +1,15 @@
+# name = input("What's your name? ").strip()
+# if "," in name:
+#     last, first = name.split(", ")
+#     name = f"{first} {last}"
+
+# print(f"hello, {name}")
+
+
+import re
+
+name = input("What's your name? ").strip()
+if matches := re.search(r"^(.+), (.+)$", name):
+    name = f"{matches.group(2)} {matches.group(1)}"
+
+print(f"hello, {name}")
---
+++ CS50_Python/Object-Oriented Programming/hat.py
@@ -0,0 +1,24 @@
+import random
+# class Hat:
+#     def __init__(self):
+#         self.houses = ["Griffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+
+#     def sort(self, name):
+#         print(name, "is in", random.choice(self.houses))
+
+
+
+# hat = Hat()
+# hat.sort("Harry")
+
+
+class Hat:
+    houses = ["Griffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]
+    @classmethod
+    def sort(cls, name):
+        print(name, "is in", random.choice(cls.houses))
+
+Hat.sort("Harry")
+Hat.sort("Heromioni")
+Hat.sort("Ron")
+Hat.sort("Draco")
