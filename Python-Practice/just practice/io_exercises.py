4eee4f0c01732a4c1b0f86e9f7a464cf00244257
KaushalCXXAI <codecrusher0@gmail.com>
2025-11-24 22:54:20 +0530

    practiced some basic exercises from PyNative

---
+++ Python-Practice/just practice/io_exercises.py
@@ -0,0 +1,40 @@
+def exercise1():
+    a = int(input("Enter First Number: "))
+    b = int(input("Enter Second Number: "))
+    ans = a * b
+    print(f"product of {a} and {b} is {ans}")
+
+# exercise1()
+
+def exercise2():
+    str1 = 'My'
+    str2 = 'Name'
+    str3 = 'Is'
+    str4 = 'James'
+    print(str1, str2, str3, str4,sep="**")
+
+# exercise2()
+
+def exercise3():
+    num = 10
+    print(format(num, 'o'))
+    print(oct(num))
+    print('%o' % num)
+    print(f"{num:o}")
+
+# exercise3()
+
+def exercise4():
+    num = 458.541315
+    print('%.2f' % num)
+
+# exercise4()
+
+def exercise5():
+    my_list = []
+    for i in range(1,6):
+        my_list.append(int(input(f"Enter Number{i} : ")))
+    print(my_list)
+
+# exercise5()
+
